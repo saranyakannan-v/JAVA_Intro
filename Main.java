ad31cddcbcfe2e766916d913daf25c35222033c1
saranyakannan-v <saranyakannan.v@gmail.com>
2021-02-24 16:35:05 +0100

    datatype

---
+++ Main.java
@@ -0,0 +1,23 @@
+// In Java, every application begins with a class name, and that class must match the filename. 
+// The name of the java file must match the class name. When saving the file, save it using the class name and add ".java" to the end of the filename.
+// The main() method is required and you will see it in every Java program.
+
+/* public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}*/
+
+/*================DATA TYPE================= */ 
+
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.78;
+    int myInt = (int) myDouble; // Manual casting: double to int
+
+    System.out.println(myDouble);   // Outputs 9.78
+    System.out.println(myInt);      // Outputs 9
+  }
+}
+
+
